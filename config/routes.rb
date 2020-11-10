27609fb5271ecd7a3ccad9920277ab05eeb3755f
syrengr <namasaya.sayuri0412@gmail.com>
2020-11-10 11:47:52 +0900

    Update home.html.erb and help.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  # /static_pages/homeというURLに対するリクエストを、StaticPagesコントローラのhomeアクションと結びつけている
   get 'static_pages/home'
-
+  # /static_pages/helpというURLに対するリクエストを、StaticPagesコントローラのhelpアクションと結びつけている
   get 'static_pages/help'
-
   root 'application#hello'
 end
