afdaa7761368d4ee058f3803b7a2ae8ec9a4aadf
syrengr <namasaya.sayuri0412@gmail.com>
2020-11-10 15:29:28 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  # ルート「/」へのGETリクエストがStaticPagesコントローラのhomeアクションにルーティングされる
+  root 'static_pages#home'
   # /static_pages/homeというURLに対するリクエストを、StaticPagesコントローラのhomeアクションと結びつけている
   get 'static_pages/home'
   # /static_pages/helpというURLに対するリクエストを、StaticPagesコントローラのhelpアクションと結びつけている
   get 'static_pages/help'
+  # /static_pages/aboutというURLに対するリクエストを、StaticPagesコントローラのaboutアクションと結びつけている
   get 'static_pages/about'
-  root 'application#hello'
+  # /static_pages/contactというURLに対するリクエストを、StaticPagesコントローラのcontactアクションと結びつけている
+  get 'static_pages/contact'
 end
--- spec/requests/access_to_static_pages_spec.rb
+++ spec/requests/access_to_static_pages_spec.rb
@@ -2,6 +2,13 @@ require "rails_helper"
 
 RSpec.describe "Access to static_pages", type: :request do
   context "Access to static pages" do
+    it "should get root" do
+      # アクションをgetして正常に動作することを確認する
+      get root_url
+      # 200レスポンスが返却されていることを確認する
+      expect(response).to have_http_status 200
+    end
+
     it "should get home" do
       # アクションをgetして正常に動作することを確認する
       get static_pages_home_url
@@ -27,6 +34,15 @@ RSpec.describe "Access to static_pages", type: :request do
       expect(response).to have_http_status 200
       # 文字列を含むレスポンスを返していることを確認する
       expect(response.body).to include "About | Ruby on Rails Tutorial Sample App"
+    end
+
+    it "should get contact" do
+      # アクションをgetして正常に動作することを確認する
+      get static_pages_contact_url
+      # 200レスポンスが返却されていることを確認する
+      expect(response).to have_http_status 200
+      # 文字列を含むレスポンスを返していることを確認する
+      expect(response.body).to include "Contact | Ruby on Rails Tutorial Sample App"
     end
   end
 end
