2236ab3d5df7a833f95e680248bead046bb49887
syrengr <namasaya.sayuri0412@gmail.com>
2020-11-24 18:27:53 +0900

    Catch error test

--- spec/requests/access_to_users_spec.rb
+++ spec/requests/access_to_users_spec.rb
@@ -1,43 +1,56 @@
 # rails_helperを読み込む
 require "rails_helper"
 
-# タイトルの引用元：Rails チュートリアル（３章、４章、５章）をRSpecでテスト
+# タイトルの引用元：Rails チュートリアル（7章）をRSpecでテスト
 RSpec.describe "access to users", type: :request do
-  # タイトルの引用元：Rails チュートリアル（３章、４章、５章）をRSpecでテスト
+  # タイトルの引用元：Rails チュートリアル（7章）をRSpecでテスト
   describe "GET #new" do
-    # タイトルの引用元：Rails Tutorial
-    it "should get new" do
-      # アクションをgetして正常に動作することを検証する
-      get signup_path
-      # 200レスポンスが返却されていることを検証する
-      expect(response).to have_http_status 200
+    # タイトルの引用元：Rails チュートリアル（7章）をRSpecでテスト
+    # userが追加されるときの検証
+    it "adds a user" do
+      # 以下の挙動を期待する
+      expect do
+        # paramsの値をハッシュで取得し、signupアクションに登録する
+        post signup_path, params: { user: attributes_for(:user) }
+      # Userモデルに1user登録されていることを期待する
+      end.to change(User, :count).by(1)
+    # 以下からスコープ外
+    end
+
+    # user追加されたときの検証
+    context "adds a user" do
+      # userモデルをハッシュで取得してuserキーに探させ、paramsに代入し、signupアクションに登録する
+      before { post signup_path, params: { user: attributes_for(:user) } }
+      # responseの処理を変数に置き換える
+      subject { response }
+      # 最後のUserをリダイレクトしていることを期待する
+      it { is_expected.to redirect_to user_path(User.last) }
+      # 一時的なリダイレクト302 status codeを返していることを期待する
+      it { is_expected.to have_http_status 302 }
     end
   end
 
-  # コンセプトを明確にする意味とユーザー登録ページをダブルチェックする意味も兼ねて、両方のメソッドを呼び出す
-  # 有効なuserの検証
-  context "valid request" do
-    # userが追加される
-    it "adds a user" do
-      expect do
-        # user数を覚えた後にデータを投稿してみて、user数が変わらないかどうかを検証する
-      #   post signup_path, params: { user: attributes_for(:user) }
-      # end.to change(User, :count).by(1)
-      end
-    end
-    # userが追加されたときの検証
-    context "adds a user" do
-      # paramsの値をsignup_pathに登録する（attributes_forはハッシュで返す）
-      # before { post signup_path, params: { user: attributes_for(:user) } }
-      # レスポンスを検証する
-      # subject { response }
-      # showページにリダイレクトされることを期待する
-      # it { is_expected.to redirect_to user_path(User.last) }
-      # リダイレクトに成功することを期待する
-      # it { is_expected.to have_http_status 302 }
-      # エラーメッセージを検証する
-      # it { is_expected.to 'div#<CSS id for error explanation>' }
-      # it { is_expected.to 'div.<CSS class for field with error>' }
-    end
+  # 無効なリクエスト
+  context "invalid request" do
+    # 無効なデータを作成
+    let(:user_params) do
+      attributes_for(:user, name: "",
+                            email: "user@invalid",
+                            password: "",
+                            password_confirmation: "")
   end
+  
+  # userが追加されないときの検証
+  it "does not add a user" do
+    expect do
+      post signup_path, params: { user: user_params }
+    end.to change(User, :count).by(0)
+  end
+
+  #     # アクションをgetして正常に動作することを検証する
+  #     get signup_path
+  #     # 200レスポンスが返却されていることを検証する
+  #     expect(response).to have_http_status 200
+  #   end
+  # end
 end
